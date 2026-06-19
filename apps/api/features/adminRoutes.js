4d270f0055f0b7179315e6c3a83aa71b5cd2b74c
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-18 22:44:15 -0300

    feat: add admin route aggregator

---
+++ apps/api/features/adminRoutes.js
@@ -0,0 +1,18 @@
+const { handleMemberActions } = require('./memberActions');
+const { handlePlanActions } = require('./planActions');
+const { handleCancelActions } = require('./cancelActions');
+
+async function handleAdminRoutes(req, res, user, url, helpers) {
+  const member = await handleMemberActions(req, res, user, url, helpers);
+  if (member !== false) return member;
+
+  const plan = await handlePlanActions(req, res, user, url, helpers);
+  if (plan !== false) return plan;
+
+  const end = await handleCancelActions(req, res, user, url, helpers);
+  if (end !== false) return end;
+
+  return false;
+}
+
+module.exports = { handleAdminRoutes };
