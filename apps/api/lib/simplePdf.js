e6ea264fb80765dd590dcfb3b7c6a7de7daf1690
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-19 14:49:08 -0300

    fix: add simple pdf helper

---
+++ apps/api/lib/simplePdf.js
@@ -0,0 +1,44 @@
+function escapeText(value) {
+  return String(value ?? '')
+    .replace(/\\/g, '\\\\')
+    .replace(/\(/g, '\\(')
+    .replace(/\)/g, '\\)')
+    .replace(/[\r\n]+/g, ' ');
+}
+
+function buildSimplePdf(title, lines = []) {
+  const safeTitle = escapeText(title);
+  const contentLines = [safeTitle, '', ...lines.map(escapeText)];
+  const streamParts = ['BT', '/F1 18 Tf', '50 790 Td', `(${contentLines[0]}) Tj`, '/F1 11 Tf'];
+  let y = 0;
+  for (const line of contentLines.slice(1)) {
+    y -= 18;
+    if (y < -720) break;
+    streamParts.push(`0 -18 Td (${line}) Tj`);
+  }
+  streamParts.push('ET');
+  const stream = streamParts.join('\n');
+
+  const objects = [
+    '1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n',
+    '2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n',
+    '3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Resources << /Font << /F1 4 0 R >> >> /Contents 5 0 R >>\nendobj\n',
+    '4 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n',
+    `5 0 obj\n<< /Length ${Buffer.byteLength(stream)} >>\nstream\n${stream}\nendstream\nendobj\n`
+  ];
+
+  let pdf = '%PDF-1.4\n';
+  const offsets = [0];
+  for (const object of objects) {
+    offsets.push(Buffer.byteLength(pdf));
+    pdf += object;
+  }
+  const xrefOffset = Buffer.byteLength(pdf);
+  pdf += `xref\n0 ${objects.length + 1}\n`;
+  pdf += '0000000000 65535 f \n';
+  for (const offset of offsets.slice(1)) pdf += `${String(offset).padStart(10, '0')} 00000 n \n`;
+  pdf += `trailer\n<< /Size ${objects.length + 1} /Root 1 0 R >>\nstartxref\n${xrefOffset}\n%%EOF`;
+  return Buffer.from(pdf, 'utf8');
+}
+
+module.exports = { buildSimplePdf };
