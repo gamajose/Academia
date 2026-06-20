f081b91fa9a1654ec78b2208a07d8d6eee28e774
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-20 13:22:38 -0300

    feat: add memberships module to navigation

--- apps/web/nav.js
+++ apps/web/nav.js
@@ -14,6 +14,7 @@ function renderNavigation() {
     ['painel.html', 'Painel'],
     ['alunos.html', 'Alunos'],
     ['planos.html', 'Planos'],
+    ['vinculos.html', 'Matrículas'],
     ['financeiro.html', 'Financeiro'],
     ['alerts.html', 'Alertas'],
     ['training.html', 'Treinos'],
