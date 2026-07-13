bc168624c62c894a5f70a35a47bd902a8d78945b
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-07-13 13:55:39 -0300

    fix: paginar relatorios em PDF

--- apps/api/lib/simplePdf.js
+++ apps/api/lib/simplePdf.js
@@ -8,24 +8,36 @@ function escapeText(value) {
 
 function buildSimplePdf(title, lines = []) {
   const safeTitle = escapeText(title);
-  const contentLines = [safeTitle, '', ...lines.map(escapeText)];
-  const streamParts = ['BT', '/F1 18 Tf', '50 790 Td', `(${contentLines[0]}) Tj`, '/F1 11 Tf'];
-  let y = 0;
-  for (const line of contentLines.slice(1)) {
-    y -= 18;
-    if (y < -720) break;
-    streamParts.push(`0 -18 Td (${line}) Tj`);
+  const normalizedLines = lines.map(escapeText);
+  const pageChunks = [];
+  for (let index = 0; index < normalizedLines.length || index === 0; index += 38) {
+    pageChunks.push(normalizedLines.slice(index, index + 38));
   }
-  streamParts.push('ET');
-  const stream = streamParts.join('\n');
 
   const objects = [
     '1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n',
-    '2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n',
-    '3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Resources << /Font << /F1 4 0 R >> >> /Contents 5 0 R >>\nendobj\n',
-    '4 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n',
-    `5 0 obj\n<< /Length ${Buffer.byteLength(stream)} >>\nstream\n${stream}\nendstream\nendobj\n`
+    ''
   ];
+  const pageObjectNumbers = [];
+  for (const chunk of pageChunks) {
+    const pageObjectNumber = objects.length + 1;
+    const contentObjectNumber = pageObjectNumber + 1;
+    const streamParts = ['BT', '/F1 18 Tf', '50 790 Td', `(${safeTitle}) Tj`, '/F1 11 Tf'];
+    for (const line of chunk) streamParts.push(`0 -18 Td (${line}) Tj`);
+    streamParts.push('ET');
+    const stream = streamParts.join('\n');
+    pageObjectNumbers.push(pageObjectNumber);
+    objects.push(`PAGE_${pageObjectNumber}`);
+    objects.push(`${contentObjectNumber} 0 obj\n<< /Length ${Buffer.byteLength(stream)} >>\nstream\n${stream}\nendstream\nendobj\n`);
+  }
+
+  const fontObjectNumber = objects.length + 1;
+  objects.push(`${fontObjectNumber} 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n`);
+  objects[1] = `2 0 obj\n<< /Type /Pages /Kids [${pageObjectNumbers.map((number) => `${number} 0 R`).join(' ')}] /Count ${pageObjectNumbers.length} >>\nendobj\n`;
+  for (const pageObjectNumber of pageObjectNumbers) {
+    const contentObjectNumber = pageObjectNumber + 1;
+    objects[pageObjectNumber - 1] = `${pageObjectNumber} 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Resources << /Font << /F1 ${fontObjectNumber} 0 R >> >> /Contents ${contentObjectNumber} 0 R >>\nendobj\n`;
+  }
 
   let pdf = '%PDF-1.4\n';
   const offsets = [0];
