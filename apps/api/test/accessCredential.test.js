0437cd9ad16acaf248447647aab336469861e6b8
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-07-12 21:50:52 -0300

    test: validar normalizacao do codigo dinamico

---
+++ apps/api/test/accessCredential.test.js
@@ -0,0 +1,14 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { normalizeAccessCode } = require('../features/accessRoutes');
+
+test('normaliza codigo dinamico com espaco ou hifen', () => {
+  assert.equal(normalizeAccessCode('483 921'), '483921');
+  assert.equal(normalizeAccessCode('483-921'), '483921');
+});
+
+test('rejeita codigo com quantidade incorreta de digitos', () => {
+  assert.equal(normalizeAccessCode('12345'), '');
+  assert.equal(normalizeAccessCode('1234567'), '');
+  assert.equal(normalizeAccessCode(''), '');
+});
